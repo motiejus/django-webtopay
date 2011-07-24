2fb8849aeec278f9234786cc567dbc492e2396e3
Motiejus Jakštys <desired.mta@gmail.com>
2011-07-24 22:35:12 +0300

    Improving test infrastructure

---
+++ runtests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys
+
+from os.path import dirname, abspath
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        ROOT_URLCONF='',
+        INSTALLED_APPS=['webtopay'],
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3'
+                }
+            }
+    )
+
+from django.test.simple import DjangoTestSuiteRunner
+
+def runtests():
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+    testrunner = DjangoTestSuiteRunner()
+    failures = testrunner.run_tests(None)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    runtests(*sys.argv[1:])
