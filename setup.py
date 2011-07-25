dffb3d5bbf0a549129873dd3ea2c185872536c85
Motiejus Jakštys <desired.mta@gmail.com>
2011-07-26 00:57:48 +0300

    Attempting to make valid setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,15 @@ from setuptools import setup, find_packages
 
 import webtopay
 
-install_requires = [
-    'Django>=1.3',
-    'M2crypto'
-    ]
-
 setup(
     name='django-webtopay',
     version=".".join(map(str, webtopay.__version__)),
     author='Motiejus Jakštys',
     author_email='desired.mta@gmail.com',
     url='https://github.com/Motiejus/django-webtopay',
-    install_requires=install_requires,
+    install_requires=['Django>=1.3', 'M2crypto'],
     description = 'A pluggable Django application for integrating WebToPay Payments',
-    packages=find_packages(),
-    include_package_data=True,
+    packages=['webtopay'],
     classifiers=[
         "Framework :: Django",
         "Intended Audience :: Developers",
