826b73996b8be03fec572667cd3d1b7715755b01
Simas Skrebiškis <simas.skrebiskis@gmail.com>
2014-04-15 12:07:56 +0300

    fix wrong urls module name

--- webtopay/urls/makro.py
+++ webtopay/urls/makro.py
@@ -3,7 +3,7 @@ import django
 if django.VERSION >= (1, 6):
     from django.conf.urls import patterns, url
 else:
-    from django.conf.defaults.urls import patterns, url
+    from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('webtopay.views',
